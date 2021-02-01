f346449d03a15259db95b4204f80f75dd9f7d044
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-01 15:38:50 +0200

    Add files via upload

---
+++ Task01_Even_Lines/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task01_Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToRead = Path.Combine("data", "Text.txt");
+
+            Regex replace = new Regex(@"-|,|\.|!|\?");
+
+            using(StreamReader readFromFile = new StreamReader(pathToRead))
+            {
+                int lineCounter = -1;
+                while (true)
+                {
+                    
+                    string nextLineRaw = readFromFile.ReadLine();
+                    if(nextLineRaw == null)
+                    {
+                        break;
+                    }
+                    
+                    string[] nextLine = nextLineRaw.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    lineCounter++;
+
+                    if(lineCounter % 2 == 0)
+                    {
+                        for (int i = nextLine.Length - 1; i >= 0; i--)
+                        {
+                            nextLine[i] = replace.Replace(nextLine[i], "@");
+                            Console.Write(nextLine[i]);
+
+                            if(i > 0)
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+
+
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
