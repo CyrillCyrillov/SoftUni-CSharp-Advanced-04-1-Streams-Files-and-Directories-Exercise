0cdf6191f3d7628a6ccadb8c392fef24cab18f2e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-01 22:29:08 +0200

    Add files via upload

---
+++ Task06_Zip_and_Extract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Task06_Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToZip = Path.Combine("data", "copyMe.png");
+            string pathToExtract = Path.Combine("extract");
+
+            using (ZipArchive zipFile = ZipFile.Open("copyMe.zip", ZipArchiveMode.Create))
+            {
+                ZipArchiveEntry zipArchiveEntry = zipFile.CreateEntryFromFile(pathToZip, "copyMe.png");
+            }
+        }
+    }
+}
